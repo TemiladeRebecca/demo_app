f0b65290946d77b9768ab35e89b0564729c8feb7
TemiladeRebecca <temilade.rebecca20@gmail.com>
2025-09-16 00:19:39 +0100

    feat(ui): add BaseScreen widget with appBar, padding, and scrollable options

---
+++ lib/custom/base_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class BaseScreen extends StatelessWidget {
+  final Widget child;
+  final PreferredSizeWidget? appBar;
+  final bool scrollable;
+  final EdgeInsetsGeometry padding;
+
+  const BaseScreen({
+    super.key,
+    required this.child,
+    this.appBar,
+    this.scrollable = false,
+    this.padding = const EdgeInsets.all(16.0),
+  });
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: appBar,
+      body: SafeArea(
+        child: Padding(
+          padding: padding,
+          child: scrollable
+              ? SingleChildScrollView(child: child)
+              : child,
+        ),
+      ),
+    );
+  }
+}
