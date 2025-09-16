5716d1482db2edc6590cec2efffcdf4b938b197c
TemiladeRebecca <temilade.rebecca20@gmail.com>
2025-09-16 16:37:42 +0100

    chore: update sample_image.dart with new image URLs and remove unused description property

---
+++ lib/data/sample_image.dart
@@ -0,0 +1,34 @@
+import 'package:my_app/widgets/image_item.dart';
+
+final List<ImageItem> sampleImages = [
+  ImageItem(
+    id: uuid.v4(),
+    imageUrl: "https://cdn.pixabay.com/photo/2024/07/19/12/23/woman-8906207_640.jpg",
+    name: "Display",
+  ),
+  ImageItem(
+    id: uuid.v4(),
+    imageUrl: "https://cdn.pixabay.com/photo/2025/01/23/10/59/ai-generated-9354449_640.jpg",
+    name: "Promotion",
+  ),
+  ImageItem(
+    id: uuid.v4(),
+    imageUrl: "https://cdn.pixabay.com/photo/2024/05/29/21/11/woman-8797240_640.png",
+    name: "Branding",
+  ),
+  ImageItem(
+    id: uuid.v4(),
+    imageUrl: "https://cdn.pixabay.com/photo/2024/02/20/08/45/woman-8584979_640.jpg",
+    name: "Promotion",
+  ),
+  ImageItem(
+    id: uuid.v4(),
+    imageUrl: "https://cdn.pixabay.com/photo/2024/04/12/15/46/beautiful-8692180_640.png",
+    name: "Product",
+  ),
+  ImageItem(
+    id: uuid.v4(),
+    imageUrl: "https://cdn.pixabay.com/photo/2023/08/17/05/55/ai-generated-8195533_640.png",
+    name: "Branding",
+  ),
+];
